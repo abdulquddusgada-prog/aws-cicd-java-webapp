669f1134ffd64879cd6c0370698f4c16e119d845
Abdul Quddus <abdulquddusgada@gmail.com>
2026-08-10 07:54:17 +0000

    Add Maven Java application and Jenkins CI pipeline

---
+++ src/main/java/com/devops/app/App.java
@@ -0,0 +1,9 @@
+package com.devops.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("AWS CI/CD Java Web Application");
+        System.out.println("Build and deployment pipeline is ready.");
+    }
+}
